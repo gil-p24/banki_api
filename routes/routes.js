7d70d70c50f0452bd59d74d58bf2d3d9d5ebe563
Gilbert Polanco <gilbertpolanco@GilbertancosMBP.lan>
2022-07-02 12:23:30 -0400

    building up

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 const router = Router()
+import bankRoutes from './bankRoutes.js'
 
 router.get("/", (req,res)=>{
   res.json({"message":"WASSUPP ITS WORKING"})
 })
+
+router.use("/question", bankRoutes)
 
 export default router
