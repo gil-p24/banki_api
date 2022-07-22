6979551c83c54bb83cbf1dbc78bd3b01e53d919c
Gilbert Polanco <gilbertpolanco@GilbertancosMBP.lan>
2022-07-21 20:54:21 -0400

    add assert

--- data/seed.js
+++ data/seed.js
@@ -1,6 +1,6 @@
 import db from "../connection.js";
 import question from "../models/models.js";
-import data from "./questions.json" 
+import data from "./questions.json" assert {type: "json"}
 
 
 const seed = async () => {
