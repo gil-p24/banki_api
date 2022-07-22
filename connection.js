ac652a38ec1f3a36777e68e6e542f2449ccecd00
Gilbert Polanco <gilbertpolanco@GilbertancosMBP.lan>
2022-07-22 11:44:08 -0400

    throwing shit

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ import chalk from "chalk";
 
 
 let connectionString = process.env.DB_URL || "mongodb://127.0.0.1:27017/Bank";
-let mongooseConfig = { useNewUrlParser: true, useUnifiedTopology: true };
+// let mongooseConfig = { useNewUrlParser: true, useUnifiedTopology: true };
 
 mongoose.connect(connectionString, mongooseConfig);
 
