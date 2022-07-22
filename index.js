0bebe8451aa9fd929ccb5a7f02adeb95273fb45f
Gilbert Polanco <gilbertpolanco@GilbertancosMBP.lan>
2022-07-22 10:53:05 -0400

    freakin port

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ import chalk from "chalk";
 import router from "./routes/routes.js";
 
 const app = express()
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000
 app.use(express.json());
 app.use(cors());
 app.use("/", router);
